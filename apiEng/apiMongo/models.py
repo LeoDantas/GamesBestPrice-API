f822ea7b53d580349f22be6802e61fdbbdcd075e
Unknown <leodantas96@gmail.com>
2018-07-10 17:38:05 -0300

    API finalizada

---
+++ apiEng/apiMongo/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from mongoengine import Document,fields
+
+class Game(Document):
+    official = fields.StringField(required=False)
+    name = fields.StringField(required=False)
+    price = fields.FloatField(required=False)
+
+    priceNu = fields.FloatField(required=False)
+    priceSteam = fields.FloatField(required=False)
+    priceG2a = fields.FloatField(required=False)
+
+    linkNu = fields.StringField(required=False)
+    linkSteam = fields.StringField(required=False)
+    linkG2a = fields.StringField(required=False)
+    
+    img = fields.StringField(required=False)
+
+
+# Create your models here.
+# cria as tabelas
+# cria modelos de dados
---
+++ apiEng/apiMongo/views.py
@@ -0,0 +1,52 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework_mongoengine import viewsets
+from apiMongo.serializers import GameSerializer
+from .models import Game
+from rest_framework import generics, status
+from django_filters.rest_framework import FilterSet, filters
+from django.http import Http404
+import mongoengine
+from django_filters import rest_framework
+
+# controlres = espera a acao pra agir
+# Create your views here.
+
+@api_view(['GET'])
+def index(request, format=None):
+    return Response({
+        'api': 'API Mongo ENGENHARIA',
+        'version': '1.0',
+    })
+
+class GameViewSet(viewsets.ModelViewSet):
+    lookup_field = 'id'
+    serializer_class = GameSerializer
+
+    # Filtering by id,name,price and link
+    def get_queryset(self):
+        queryset = Game.objects.all()
+        idGame = self.request.query_params.get('id',None)
+        nameGame = self.request.query_params.get('name',None)
+        priceGame = self.request.query_params.get('price',None)
+        linkGame = self.request.query_params.get('link',None)
+        officialName = self.request.query_params.get('official',None)
+
+        if idGame is not None:
+            queryset = queryset.filter(id = idGame)
+        if nameGame is not None:
+            queryset = queryset.filter(name__icontains = nameGame)
+        if priceGame is not None:
+            queryset = queryset.filter(price__lte = priceGame)
+        if officialName is not None:
+            queryset = queryset.filter(official = officialName)
+        return queryset
+
+    # Implements the DELETE
+    def delete(self,request):
+        self.get_queryset().delete()    
+        return Response()
+
+
+
